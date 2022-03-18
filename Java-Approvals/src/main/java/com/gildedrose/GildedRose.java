7c86fd663f08a90c42a869a99f8c5f0374d8ba20
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-03-18 13:58:18 +0000

    Invert another if to check for Sulfuras

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -28,7 +28,8 @@ class GildedRose {
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         item.quality = 0;
                     } else if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        } else {
                             item.quality = item.quality - 1;
                         }
                     }
