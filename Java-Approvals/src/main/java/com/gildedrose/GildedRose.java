f6c5c630c06122d4ac005556443d3762bed529bb
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-03-18 13:04:52 +0000

    Replace for loop with enhanced for

--- Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Approvals/src/main/java/com/gildedrose/GildedRose.java
@@ -8,9 +8,7 @@ class GildedRose {
     }
 
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            Item item = items[i];
-
+        for (Item item : items) {
             if (item.name.equals("Aged Brie")) {
                 updateForAgedBrie(item);
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
