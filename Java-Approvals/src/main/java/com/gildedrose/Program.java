54044b25f46dfb7a42ed1f53ea3000f957f1aeea
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-04-01 14:03:44 +0100

    Fix indentation in Program.java

--- Java-Approvals/src/main/java/com/gildedrose/Program.java
+++ Java-Approvals/src/main/java/com/gildedrose/Program.java
@@ -2,30 +2,31 @@ package com.gildedrose;
 
 public class Program {
 
-	public static void main(String... args) {
-		System.out.println("OMGHAI!");
+    public static void main(String... args) {
+        System.out.println("OMGHAI!");
 
-		Item[] items = new Item[] {
-				new UpdatableItem("+5 Dexterity Vest", 10, 20),
-				new AgedBrie(2, 0),
-				new UpdatableItem("Elixir of the Mongoose", 5, 7),
-				new Sulfuras(0),
-				new Sulfuras(-1),
-				new BackstagePasses(15, 20),
-				new BackstagePasses(10, 49),
-				new BackstagePasses(5, 49),
-				new ConjuredItem("Conjured Mana Cake", 3, 6) };
+        Item[] items = new Item[]{
+            new UpdatableItem("+5 Dexterity Vest", 10, 20),
+            new AgedBrie(2, 0),
+            new UpdatableItem("Elixir of the Mongoose", 5, 7),
+            new Sulfuras(0),
+            new Sulfuras(-1),
+            new BackstagePasses(15, 20),
+            new BackstagePasses(10, 49),
+            new BackstagePasses(5, 49),
+            new ConjuredItem("Conjured Mana Cake", 3, 6)
+        };
 
-		GildedRose app = new GildedRose(items);
+        GildedRose app = new GildedRose(items);
 
-		for (int i = 0; i < 31; i++) {
-			System.out.println("-------- day " + i + " --------");
-			System.out.println("name, sellIn, quality");
-			for (int j = 0; j < items.length; j++) {
-				System.out.println(items[j]);
-			}
-			System.out.println("");
-			app.updateQuality();
-		}
-	}
+        for (int i = 0; i < 31; i++) {
+            System.out.println("-------- day " + i + " --------");
+            System.out.println("name, sellIn, quality");
+            for (int j = 0; j < items.length; j++) {
+                System.out.println(items[j]);
+            }
+            System.out.println("");
+            app.updateQuality();
+        }
+    }
 }
