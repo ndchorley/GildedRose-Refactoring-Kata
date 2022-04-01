2875a4798738a80fe4647490501c310cad32d9b6
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-04-01 10:02:14 +0100

    Introduce an AgedBrie subclass of Item
    GildedRose's updateQuality is now in a state where we can
    see polymorphism. Let's see how we can get there.

--- Java-Approvals/src/main/java/com/gildedrose/Program.java
+++ Java-Approvals/src/main/java/com/gildedrose/Program.java
@@ -5,10 +5,10 @@ public class Program {
 	public static void main(String... args) {
 		System.out.println("OMGHAI!");
 
-		Item[] items = new Item[] { 
-				new Item("+5 Dexterity Vest", 10, 20), 
-				new Item("Aged Brie", 2, 0),
-				new Item("Elixir of the Mongoose", 5, 7), 
+		Item[] items = new Item[] {
+				new Item("+5 Dexterity Vest", 10, 20),
+				new AgedBrie(2, 0),
+				new Item("Elixir of the Mongoose", 5, 7),
 				new Item("Sulfuras, Hand of Ragnaros", 0, 80),
 				new Item("Sulfuras, Hand of Ragnaros", -1, 80),
 				new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
