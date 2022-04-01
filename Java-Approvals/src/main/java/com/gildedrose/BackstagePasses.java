86e0abd7d0981d457324bcc4dacad6b91d843e8d
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2022-04-01 10:19:01 +0100

    Introduce BackstagePasses as a subclass of Item

---
+++ Java-Approvals/src/main/java/com/gildedrose/BackstagePasses.java
@@ -0,0 +1,7 @@
+package com.gildedrose;
+
+public class BackstagePasses extends Item {
+    public BackstagePasses(int sellIn, int quality) {
+        super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+    }
+}
--- Java-Approvals/src/main/java/com/gildedrose/Program.java
+++ Java-Approvals/src/main/java/com/gildedrose/Program.java
@@ -11,9 +11,9 @@ public class Program {
 				new Item("Elixir of the Mongoose", 5, 7),
 				new Item("Sulfuras, Hand of Ragnaros", 0, 80),
 				new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-				new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-				new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-				new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+				new BackstagePasses(15, 20),
+				new BackstagePasses(10, 49),
+				new BackstagePasses(5, 49),
 				// this conjured item does not work properly yet
 				new Item("Conjured Mana Cake", 3, 6) };
 
